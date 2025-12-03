d86355ee3b5d60d74fee8310e24ebde17b9f4004
Florian Doetzl <e12402358@student.tuwien.ac.at>
2025-12-03 19:57:10 +0100

    Added Crank-Nicolson

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -57,35 +57,61 @@ namespace ASC_ode
 
   class ImprovedEuler : public TimeStepper 
   {
-public:
-  explicit ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs_in)
-    : TimeStepper(std::move(rhs_in)) {}
+  public:
+    explicit ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs_in)
+      : TimeStepper(std::move(rhs_in)) {}
 
-  void doStep(double tau, VectorView<double> y) override
+    void doStep(double tau, VectorView<double> y) override
+    {
+      // f(y_n)
+      Vector<> fyn(m_rhs->dimF());
+      m_rhs->evaluate(y, fyn);
+
+      // y~ = y_n + (tau/2) * f(y_n)
+      Vector<> ytilde(m_rhs->dimX());
+      ytilde = y;
+      for (size_t i = 0; i < ytilde.size(); ++i)
+        ytilde(i) += 0.5 * tau * fyn(i);
+
+      // f(y~)
+      Vector<> fytilde(m_rhs->dimF());
+      m_rhs->evaluate(ytilde, fytilde);
+
+      // y_{n+1} = y_n + tau * f(y~)
+      for (size_t i = 0; i < y.size(); ++i)
+        y(i) += tau * fytilde(i);
+    }
+  };
+
+  class CrankNicolson : public TimeStepper
   {
-    // f(y_n)
-    Vector<> fyn(m_rhs->dimF());
-    m_rhs->evaluate(y, fyn);
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau_half;
+    std::shared_ptr<ConstantFunction> m_yold;
+    std::shared_ptr<ConstantFunction> m_rhs_2;
 
-    // y~ = y_n + (tau/2) * f(y_n)
-    Vector<> ytilde(m_rhs->dimX());
-    ytilde = y;
-    for (size_t i = 0; i < ytilde.size(); ++i)
-      ytilde(i) += 0.5 * tau * fyn(i);
+  public:
+    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), m_tau_half(std::make_shared<Parameter>(0.0)) 
+    {
+      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+      m_rhs_2 = std::make_shared<ConstantFunction>(m_rhs->dimF());
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+      m_equ = ynew - m_yold - m_tau_half * (m_rhs_2 + m_rhs);
+    }
 
-    // f(y~)
-    Vector<> fytilde(m_rhs->dimF());
-    m_rhs->evaluate(ytilde, fytilde);
+    void doStep(double tau, VectorView<double> y) override
+    {
+      m_yold->set(y);
+      m_tau_half->set(tau * 0.5);
 
-    // y_{n+1} = y_n + tau * f(y~)
-    for (size_t i = 0; i < y.size(); ++i)
-      y(i) += tau * fytilde(i);
-  }
-};
+      Vector<> fyn(m_rhs->dimF());
+      m_rhs->evaluate(y, fyn);
+      m_rhs_2->set(fyn);
 
-
-  
-
+      NewtonSolver(m_equ, y);
+    }
+  };  
 }
 
 
